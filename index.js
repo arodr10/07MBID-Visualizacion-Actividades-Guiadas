2d99fa9adfebd87d106106cbffc0e72101afddc8
arodr10 <afrodriguezm1@gmail.com>
2022-07-09 13:47:10 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Import stylesheets
 import './style.css';
 
+// Determina el top
+var TOP = 5;
+
+const url = "https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/movies.csv";
+
+// Función para obtener subarrays
+
+function  getSubarray(array, fromIndex, toIndex) {
+  return array.slice(fromIndex, toIndex + 1);
+}
+
