97409f0bcccdadc541182e44c5358160b18f7f21
arodr10 <afrodriguezm1@gmail.com>
2022-07-09 14:21:19 -0500

    1 file modified

--- index.js
+++ index.js
@@ -16,40 +16,16 @@ var visualization = d3plus
   .y("value")
   .draw();
 
-  /*
-  d3.json("https://raw.githubusercontent.com/ raul27868/07MBIG-Visualizacion-Actividades-Guiadas/ master/columnas.json", function(data) { 
-      var visualization = d3plus
-        .viz()
-        .container("#viz2")
-        .data(data)
-        .type('bar')
-        .id('name')
-        .x('year')
-        .y('value')
-        .axes({ ticks: 'false' })
-        .draw(); 
-    });
-*/
-    d3.json("https://raw.githubusercontent.com/ raul27868/07MBIG-Visualizacion-Actividades-Guiadas/ master/columnas.json",function (data) {
-    
-      var visualization = d3plus
-    
-        .viz()
-    
-        .container('#viz3')
-    
-        .data(data)
-    
-        .type('bar')
-    
-        .id('name')
-    
-        .x('year')
-    
-        .y('value')
-    
-        .axes({ ticks: 'false' })
-    
-        .draw();
-    
-    });
+  d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json",function (data) {    
+    var visualization = d3plus    
+      .viz()    
+      .container('#viz2')    
+      .data(data)
+      .type('bar')    
+      .id('name')    
+      .x('year')    
+      .y('value')
+      .axes({ ticks: 'false' })
+      .draw();
+  
+  });
